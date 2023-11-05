09ed04ddc35d25e1fa8fa75e83959b9601339fbc
Lydie Danet <danet.lydie@gmail.com>
2023-11-05 00:26:27 -0400

    Ensure showcase moves every player across a tile

--- src/components/TileShowcase/TileShowcase.tsx
+++ src/components/TileShowcase/TileShowcase.tsx
@@ -4,6 +4,8 @@ import { colors } from "../../engine/constants";
 import { movePlayers } from "../../engine/handlers";
 import {
   Coordinate,
+  Player,
+  PlayerColor,
   Players as PlayersType,
   SharedGameState,
 } from "../../engine/types";
@@ -43,65 +45,116 @@ const edgeNotchCoordinates = [
   ...leftRightNotchCoordinates,
 ];
 
+const generateBoard = () => {
+  const shuffledTiles = shuffle([
+    ...Object.keys(tiles),
+    null,
+  ] as (TileID | null)[]);
+
+  return Array(6)
+    .fill(true)
+    .map((_, i) =>
+      Array(6)
+        .fill(true)
+        .map((_, j) => {
+          const tileID = shuffledTiles[i + 6 * j];
+          if (tileID === null) return null;
+          const tile = tiles[tileID];
+          return {
+            id: tileID,
+            combination:
+              tile.combinations[
+                Math.floor((Math.random() * 4) % tile.combinations.length)
+              ],
+          };
+        })
+    );
+};
+const isUnacceptableState = (
+  state: SharedGameState,
+  currentColor: PlayerColor
+) => {
+  return (
+    state.coloredPaths.filter(
+      (coloredPath) => coloredPath.color === currentColor
+    ).length < 2
+  );
+};
+
+const getBoardState = (): SharedGameState => {
+  const board = generateBoard();
+  const playerColors = shuffle(colors);
+  const startingNotches = shuffle(edgeNotchCoordinates);
+
+  let state: SharedGameState = {
+    players: {},
+    board,
+    playerTurnsOrder: [],
+    deck: [],
+    gamePhase: "main",
+    availableColors: [],
+    coloredPaths: [],
+    winners: [],
+  };
+
+  const players: PlayersType = playerColors.reduce(
+    (currentPlayers, color, i) => {
+      let notch = startingNotches.shift();
+      let currentPlayer: Player = {
+        color,
+        name: color,
+        status: "playing",
+        hand: [],
+        startingNotch: notch,
+      };
+
+      const initialPlayerState = {
+        ...state,
+        playerTurnsOrder: [...playerColors.slice(0, i), color],
+        players: { ...currentPlayers, [color]: currentPlayer },
+      };
+
+      let nextState: SharedGameState = {
+        ...initialPlayerState,
+        ...movePlayers(initialPlayerState),
+      };
+
+      while (notch && isUnacceptableState(nextState, color)) {
+        notch = startingNotches.shift();
+        currentPlayer = { ...currentPlayer, startingNotch: notch };
+        const newPlayerState = {
+          ...initialPlayerState,
+          players: { ...currentPlayers, [color]: currentPlayer },
+        };
+        nextState = { ...newPlayerState, ...movePlayers(newPlayerState) };
+      }
+      if (!notch) return currentPlayers;
+      return {
+        ...currentPlayers,
+        [color]: { ...currentPlayer, startingNotch: notch },
+      };
+    },
+    {} as PlayersType
+  );
+
+  return {
+    ...state,
+    players,
+    playerTurnsOrder: playerColors,
+  };
+};
+
 type TileShowcaseProps = Omit<
   ComponentProps<typeof Players>,
   "players" | "coloredPaths"
 >;
 
 const TileShowcase = ({ ...playersProps }: TileShowcaseProps) => {
-  const board = useMemo(() => {
-    const shuffledTiles = shuffle([
-      ...Object.keys(tiles),
-      null,
-    ] as (TileID | null)[]);
-
-    return Array(6)
-      .fill(true)
-      .map((_, i) =>
-        Array(6)
-          .fill(true)
-          .map((_, j) => {
-            const tileID = shuffledTiles[i + 6 * j];
-            if (tileID === null) return null;
-            const tile = tiles[tileID];
-            return {
-              id: tileID,
-              combination:
-                tile.combinations[
-                  Math.floor((Math.random() * 4) % tile.combinations.length)
-                ],
-            };
-          })
-      );
-  }, []);
-
   const angle = useMemo(() => Math.random(), []);
 
-  const playerColors = shuffle(colors);
-  const startingNotches = shuffle(edgeNotchCoordinates);
+  const initialState = useMemo(() => getBoardState(), []);
 
-  const [gameState, setGameState] = useState<SharedGameState>({
-    players: playerColors.reduce(
-      (players, color, i) => ({
-        ...players,
-        [color]: {
-          color,
-          name: color,
-          status: "playing",
-          hand: [],
-          startingNotch: startingNotches[i],
-        },
-      }),
-      {} as PlayersType
-    ),
-    board,
-    playerTurnsOrder: playerColors,
-    deck: [],
-    gamePhase: "main",
-    availableColors: [],
-    coloredPaths: [],
-    winners: [],
-  });
+  const [gameState, setGameState] = useState<SharedGameState>(initialState);
 
   useEffect(() => {
     setGameState((state) => ({ ...state, ...movePlayers(state) }));
@@ -121,7 +174,7 @@ const TileShowcase = ({ ...playersProps }: TileShowcaseProps) => {
       {new Array(6).fill(true).map((_, i) => (
         <Edge key={i} type="bottom" row={6} col={i} index={6 + i} />
       ))}
-      {board.map((row, ri) => (
+      {gameState.board.map((row, ri) => (
         <Fragment key={ri}>
           <Edge type="left" row={ri} col={-1} index={ri} />
           <Edge type="right" row={ri} col={6} index={17 - ri} />
